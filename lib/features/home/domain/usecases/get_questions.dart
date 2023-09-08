cda790f4d2b7b86a2cdecce9b509c965b83e65ac
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-09-08 14:57:00 -0400

    fix: get questions use case type fixed.

--- lib/features/home/domain/usecases/get_questions.dart
+++ lib/features/home/domain/usecases/get_questions.dart
@@ -1,14 +1,14 @@
 import '../../../../core/result/result.dart';
 import '../../../../core/usecases/usecase.dart';
-import '../entities/answered_quiz.dart';
+import '../entities/question.dart';
 import '../repositories/home_repository_interface.dart';
 
-class GetQuestions implements UseCase<Future<List<AnsweredQuiz>>, NoParams> {
+class GetQuestions implements UseCase<Future<List<Question>>, NoParams> {
   final HomeRepositoryInterface repository;
   GetQuestions({required this.repository});
 
   @override
-  Future<List<AnsweredQuiz>> call(NoParams params) async {
+  Future<List<Question>> call(NoParams params) async {
     final IResult response = await repository.getQuestions();
 
     if (response.isError) {
