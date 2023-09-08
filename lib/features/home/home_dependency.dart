e4488aa61edf031b5748a017c18fa9bf331a996e
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-09-08 14:56:23 -0400

    fix: home dependencies.

--- lib/features/home/home_dependency.dart
+++ lib/features/home/home_dependency.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 
+import '../../core/usecases/usecase.dart';
 import 'data/datasources/home_remote_datasource.dart';
 import 'data/repositories/home_repository.dart';
+import 'domain/entities/question.dart';
 import 'domain/repositories/home_repository_interface.dart';
-import 'domain/usecases/get_questions.dart';
+import 'domain/usecases/get_questions.dart' as usecase;
 import 'presenter/bloc/home_bloc.dart';
 
 Future<void> initHomeDependencies(GetIt sl) async {
@@ -22,8 +24,8 @@ Future<void> initHomeDependencies(GetIt sl) async {
   );
 
   // Use cases
-  sl.registerLazySingleton(
-    () => GetQuestions(repository: sl()),
+  sl.registerLazySingleton<UseCase<Future<List<Question>>, NoParams>>(
+    () => usecase.GetQuestions(repository: sl()),
   );
 
   // Bloc
